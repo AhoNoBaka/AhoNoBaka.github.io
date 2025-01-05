b091620c1e20fdc8273d58490c219388f3105dc0
AhoNoBaka <gimenoadrianjay@gmail.com>
2025-01-06 07:05:42 +0800

    Changed the projects to modals

--- script.js
+++ script.js
@@ -4,3 +4,34 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function openModal(imgSrc, text, githubLink, liveDemoLink) {
+  var modal = document.getElementById("projectModal");
+  var modalImg = document.getElementById("modalImg");
+  var modalText = document.getElementById("modalText");
+  var githubBtn = document.getElementById("githubBtn");
+  var liveDemoBtn = document.getElementById("liveDemoBtn");
+
+  modal.style.display = "block";
+  modalImg.src = imgSrc;
+  modalText.innerHTML = text;
+  githubBtn.onclick = function() {
+    window.open(githubLink, '_blank');
+  };
+  liveDemoBtn.onclick = function() {
+    window.open(liveDemoLink, '_blank');
+  };
+}
+
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function() {
+  var modal = document.getElementById("projectModal");
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  var modal = document.getElementById("projectModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
